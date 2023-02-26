08d06d7bd953f9e752f1f907e7772712272b6839
saidrebai <rebaisaid6@gmail.com>
2023-02-26 15:00:47 +0100

    updt

--- backend/routes/jobAppRouter.js
+++ backend/routes/jobAppRouter.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const uploadFile = require("../middleware/multer")
 //const multer = require("multer");
 
-router.post("/jobApp",jobAppControlles.createJobApp);
+router.post("/job",jobAppControlles.createJobApp);
 //router.post([uploadFile.single([{ name: "images" }])]);
 //router.post("/upload", upload.single("file"), products.uploadfile);
 
