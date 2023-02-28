4c087b4a9fd546c1ea61ba000b2a129f994ddee5
alamakh <you@example.com>
2023-02-28 17:10:41 +0100

    updt

--- client/src/components/signupAdmin/index.jsx
+++ client/src/components/signupAdmin/index.jsx
@@ -18,7 +18,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:8080/api/users";
+      const url = "localhost:8080/api/adminRouters/signup";
       const { data: res } = await axios.post(url, data);
       navigate("/login");
       console.log(res.message);
