606b061ad061a3b0a268256fa236d4a593281acf
alamakh <you@example.com>
2023-03-01 17:04:05 +0100

    edit image in main

--- client/src/components/Menu/menu.jsx
+++ client/src/components/Menu/menu.jsx
@@ -155,7 +155,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <Avatar src="/broken-image.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
