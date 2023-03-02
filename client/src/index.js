e4b9b7c34642385cbff2eafe1201b779879bd85e
alamakh <you@example.com>
2023-03-02 13:33:48 +0100

    update offerCard

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import{BrowserRouter} from 'react-router-dom';
 import ResponsiveAppBar from "./components/Menu/menu";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter><ResponsiveAppBar />
+root.render( 
+    <BrowserRouter>
+    <ResponsiveAppBar/>
+    <React.StrictMode>
     <App />
+     </React.StrictMode>
     </BrowserRouter>
     
-  </React.StrictMode>
+ 
 );
 
 
