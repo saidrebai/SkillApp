734acc8659d4f22775733c77574a92fef636c0b8
saidrebai <rebaisaid6@gmail.com>
2023-03-02 16:41:49 +0100

    condidatR

--- backend/routes/candidatRouters.js
+++ backend/routes/candidatRouters.js
@@ -3,5 +3,6 @@ const candidatControlles = require("../controlles/candidatControlles");
 
 router.post("/signin", candidatControlles.authentification);
 router.post("/signup", candidatControlles.signup);
+router.get("/getinfo", candidatControlles.getinfoCondidat);
 
 module.exports = router;
