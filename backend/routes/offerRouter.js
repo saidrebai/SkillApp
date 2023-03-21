02ac968658cffb5ffae252497c0668be37580341
saidrebai <rebaisaid6@gmail.com>
2023-03-21 11:08:03 +0100

    update and delete offer

--- backend/routes/offerRouter.js
+++ backend/routes/offerRouter.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 
 router.post("/offers", offersControlles.createOffer);
 router.get("/getoffer", offersControlles.getoffer);
+router.put("/updateoffer/:id", offersControlles.updateoffer);
+router.delete("/deleteOfferr/:id", offersControlles. deleteOffer);
 
 module.exports = router;
