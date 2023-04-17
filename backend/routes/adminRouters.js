9b163ba1f427a1e3944eee4e960869f3dc26d7b3
alamakh <you@example.com>
2023-04-17 14:33:13 +0100

    add delete function

--- backend/routes/adminRouters.js
+++ backend/routes/adminRouters.js
@@ -6,5 +6,6 @@ router.post("/signup", adminControlles.signup);
 router.get("/getinfoAdmin/:id", adminControlles.getinformation);
 router.put("/updateinfoAdmin/:id", adminControlles.updateInfoAdmin);
 router.get("/getAll",adminControlles.getAll);
+router.delete("/deleteadmin/:id", adminControlles.deleteAdmin)
 
 module.exports = router;
