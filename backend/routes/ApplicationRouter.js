e2f05ba1a529d2690ab58f1ffc7ee5c072626700
alamakh <alamakhlouf@outlook.com>
2023-06-02 11:15:36 +0200

    update appcon

--- backend/routes/ApplicationRouter.js
+++ backend/routes/ApplicationRouter.js
@@ -8,5 +8,7 @@ router.get("/getscoresbyid",applicationController.getApplicationById);
 router.get("/getappbyuser/:id",applicationController.getApplicationByUser);
 router.put("/updateUser/:id",applicationController.acceptApplication);
 router.put("/refuseUser/:id",applicationController.refuseApplication);
+router.get("/getaccappsbyoffers",applicationController.getAcceptedApplicationByOffers);
+router.get("/getrefappsbyoffers",applicationController.getRefusedApplicationByOffers);
 
 module.exports = router;
