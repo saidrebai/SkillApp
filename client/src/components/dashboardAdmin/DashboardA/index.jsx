8faaf68d5f3a2497976b87c46fd6287881e3b1b3
alamakh <alamakhlouf@outlook.com>
2023-06-07 16:48:21 +0200

    updt

--- client/src/components/dashboardAdmin/DashboardA/index.jsx
+++ client/src/components/dashboardAdmin/DashboardA/index.jsx
@@ -17,7 +17,7 @@ import axios from "axios";
 
 const DashboardA = () => {
   const id = localStorage.getItem("id");
-  const ids = localStorage.getItem("ids");
+  // const ids = localStorage.getItem("ids");
 
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -89,6 +89,7 @@ const DashboardA = () => {
     fetchData();
   }, [isFetch]);
 
+  const reportURL = `https://app.powerbi.com/reportEmbed?reportId=bd9f6d82-454b-4057-a402-77f504ec6824&autoAuth=true&ctid=dbd6664d-4eb9-46eb-99d8-5c43ba153c61&filter=offermodels/admin eq '${id}'`;
   return (
     <Box m="20px">
       {/* HEADER */}
@@ -229,10 +230,11 @@ const DashboardA = () => {
           </Box>
           <div className="powerbi">
             <iframe
+              src={reportURL}
               title="Final BI"
               width="1140"
               height="541.25"
-              src="https://app.powerbi.com/reportEmbed?reportId=bd9f6d82-454b-4057-a402-77f504ec6824&autoAuth=true&ctid=dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
+              // src="https://app.powerbi.com/reportEmbed?reportId=bd9f6d82-454b-4057-a402-77f504ec6824&autoAuth=true&ctid=dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
               frameborder="0"
               allowFullScreen="true"
             ></iframe>
